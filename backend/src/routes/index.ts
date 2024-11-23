07790d172420a43b5ba6525ea1999072236bdd75
Cezar A. Molinar <cezarmolinar@gmail.com>
2024-11-23 15:54:51 -0300

    initial commit backend

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { Request, Response } from "express";
+
+const router: Router = Router();
+
+router.get("/api", (req: Request, res: Response) => {
+  res.json({ message: "API funcionando!" });
+});
+
+export default router;
