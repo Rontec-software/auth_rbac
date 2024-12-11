71829fee4e318d28db1ab9a93f9c12fbcc649b00
Jhonatan Pereira <jhonatanfrade.pereira@gmail.com>
2024-12-10 22:13:43 -0300

    feat: create login button

--- Frontend/src/app/(public)/login/page.tsx
+++ Frontend/src/app/(public)/login/page.tsx
@@ -1,8 +1,28 @@
 'use client';
 import Input from '@/components/shared/Input';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  async function handleLogin() {
+    setEmailError('');
+    setPasswordError('');
+    const data = { email, password };
+    if (!email) {
+      return setEmailError('Campo obrigatório');
+    }
+    if (!password) {
+      return setPasswordError('Campo obrigatório');
+    }
+    console.log('data', data);
+  }
+
   return (
     <div className="relative h-screen">
       <div className="absolute inset-0 bg-black bg-opacity-90"></div>
@@ -21,8 +41,9 @@ export default function Login() {
           </div>
           <Input
             name="Email"
-            event={(e) => { }}
-            value=''
+            value={email}
+            event={(e) => setEmail(e.target.value)}
+            errorMessage={emailError}
             iconAfter={
               <Image src="/email.svg" width={20} height={20} alt="email" />
             }
@@ -34,14 +55,43 @@ export default function Login() {
               <Image src="/eye.svg" width={20} height={20} alt="senha" />
             }
             type="password"
-            value=''
-            event={(e) => { }}
+            value={password}
+            event={(e) => setPassword(e.target.value)}
+            errorMessage={passwordError}
           />
+          <div className="flex justify-end items-end text-[12px] p-1 text-gray-400 cursor-pointer">
+            <Link href={'http://localhost:3000/recuperarSenha'}>
+              Esqueceu a senha?
+            </Link>
+          </div>
           <button
+            onClick={handleLogin}
             className="bg-[#22C55E] text-white font-bold rounded-md p-2 mt-6 w-full"
           >
             Login
           </button>
+          <div className="flex mt-6 justify-center items-center">
+            <hr className="border-[#363638] flex-1 border-1" />
+            <span className="pl-2 pr-2 text-[12px]">ou</span>
+            <hr className="border-[#363638]  flex-1" />
+          </div>
+          <div className="flex justify-center items-center">
+            <button className="bg-[#DB4437] text-white font-bold rounded-full p-3 mt-6 w-12">
+              G
+            </button>
+          </div>
+          <div className="flex justify-center items-center text-[12px] m-1">
+            Ainda não possui uma conta?
+            <span className="text-green-600 pl-1">
+              Cadastre-se
+              <span className="cursor-pointer border-b-2 border-green-600 pl-1">
+                <Link href={'http://localhost:3000/cadastrar'}>aqui</Link>
+              </span>
+            </span>
+          </div>
+          <div className="flex justify-center items-center text-[10px] text-gray-500 m-1">
+            ou faça login pelo Google clicando no botão G acima.
+          </div>
         </div>
       </div>
     </div>
