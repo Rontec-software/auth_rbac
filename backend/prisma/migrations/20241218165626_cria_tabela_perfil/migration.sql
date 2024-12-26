e06ddb1b19e11ce890c6f76a742e4ced1c76c958
Jhonatan Pereira <jhonatanfrade.pereira@gmail.com>
2024-12-26 17:44:37 -0300

    fix: synchronize migration

--- backend/prisma/migrations/20241218165626_cria_tabela_perfil/migration.sql
+++ backend/prisma/migrations/20241218165626_cria_tabela_perfil/migration.sql
@@ -1,29 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "User";
-PRAGMA foreign_keys=on;
-
--- CreateTable
-CREATE TABLE "users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "phoneNumber" TEXT NOT NULL,
-    "profilePicture" TEXT,
-    "active" BOOLEAN NOT NULL DEFAULT true,
-    "passwordResetToken" TEXT,
-    "expPasswordResetToken" DATETIME,
-    "twoFactorAuthEnable" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
 -- CreateTable
 CREATE TABLE "profiles" (
     "id" TEXT NOT NULL PRIMARY KEY,
@@ -32,6 +6,3 @@ CREATE TABLE "profiles" (
     "active" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
