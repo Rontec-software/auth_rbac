0d46592d36e9e803640702913a2a51b873218213
Jhonatan Pereira <jhonatanfrade.pereira@gmail.com>
2025-01-05 15:33:00 -0300

    fix: remove delete pages

--- Frontend/src/app/(auth)/perfil-de-acesso/remover/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function CriarUsuario() {
-  return (
-    <div className="flex">
-      <h1>Remover Perfil</h1>
-    </div>
-  )
-}
--- Frontend/src/app/(auth)/permissao/remover/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function CriarUsuario() {
-  return (
-    <div className="flex">
-      <h1>Remover Permissão</h1>
-    </div>
-  )
-}
